44c0bab4c8a1329ea8d01ddc2871510b3677fba7
Jozsef Arato <53469079+jozsarato@users.noreply.github.com>
2022-02-04 14:37:53 +0100

    structure update

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  4 11:56:18 2022
+
+@author: jarato
+"""
+
+from setuptools import find_packages, setup
+
+setup(
+name='PyEyeSim',
+author='jozsef arato',
+version='0.1',
+packages=find_packages(),)
