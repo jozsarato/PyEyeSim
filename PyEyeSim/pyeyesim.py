5b118e431e02e777e4e6a15e3e14fc9573aee0c5
jozsarato <jozsef.arato@univie.ac.at>
2023-05-31 14:56:29 +0200

    variable name update
    clear variable names for subject and stimulus (instead of s and p)

--- PyEyeSim/pyeyesim.py
+++ PyEyeSim/pyeyesim.py
@@ -133,12 +133,12 @@ class EyeData:
             self.images[s]=Stim
         pass 
  
-    def GetFixationData(self,s,p):
-        """get X,Y fixation sequence for a participant and stimulus
+    def GetFixationData(self,subj,stim):
+        """get X,Y fixation sequence for a subject and stimulus
         output 1: array of pixel x for sequence of fixations
         output 2: array of pixel y for sequence of fixations"""
-        SubjIdx=np.nonzero(self.data['subjectID'].to_numpy()==s)  #idx for subject
-        TrialSubIdx=np.intersect1d(np.nonzero(self.data['Stimulus'].to_numpy()==p),SubjIdx) # idx for subject and painting
+        SubjIdx=np.nonzero(self.data['subjectID'].to_numpy()==subj)  #idx for subject
+        TrialSubIdx=np.intersect1d(np.nonzero(self.data['Stimulus'].to_numpy()==stim),SubjIdx) # idx for subject and painting
         FixTrialX=np.array(self.data['mean_x'].iloc[TrialSubIdx]) # get x data for trial
         FixTrialY=np.array(self.data['mean_y'].iloc[TrialSubIdx]) # get y data for trial
         return FixTrialX,FixTrialY
