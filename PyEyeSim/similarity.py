0fe5f04e8d73576de72672cafa0d7079d197b53c
Julius Duin <julius.duin@gmail.com>
2024-05-15 10:35:09 +0200

    refactored nb for readability added functions to similarity.py

---
+++ PyEyeSim/similarity.py
@@ -0,0 +1,92 @@
+import math
+from scipy.spatial.distance import cdist
+import numpy as np
+
+def angle_from_horizontal(row, prev_x, prev_y):
+    """
+    calculate the angle from the horizontal for a all given saccades
+    """
+    dx = row['mean_x'] - prev_x
+    dy = row['mean_y'] - prev_y
+    angle = math.degrees(math.atan2(dy, dx))
+    
+    # Adjust the angle to be between -90 and 90 degrees
+    if angle > 90:
+        angle -= 180
+    elif angle < -90:
+        angle += 180
+    
+    return angle
+
+def circular_distance(angle1, angle2):
+    """
+    Calculate the circular distance between two angles in degrees.
+    """
+    diff = np.abs(angle1 - angle2)
+    return np.minimum(diff, 360 - diff)
+
+def prune_sort_corr_angles(arr_1, arr_2, kind='euclidean'):
+    '''
+    between two arrays of angles (two subjects) find the mean similarity for all pairs of angles
+    prune the arrays to the len of the smaller
+    '''
+    arr_1.sort()
+    arr_2.sort()
+
+    # Find distances between each element in arr2 and arr1
+    dists = np.abs(arr_2[:, None] - arr_1)
+
+    # Find number of elements to keep
+    n_keep = min(len(arr_1), len(arr_2))
+
+    # Drop farthest elements from both arrays
+    keep_idx1 = np.argsort(np.min(dists, axis=0))[:n_keep]
+    keep_idx2 = np.argsort(np.min(dists, axis=1))[:n_keep]
+
+    arr1_pruned = np.sort(arr_1[keep_idx1])
+    arr2_pruned = np.sort(arr_2[keep_idx2])
+
+    angles1_rad = np.deg2rad(arr1_pruned)
+    angles2_rad = np.deg2rad(arr2_pruned)
+ 
+    distances = np.array([circular_distance(a1, a2) for a1, a2 in zip(angles1_rad, angles2_rad)])
+    # Calculate pairwise circular distances using cdist
+    return distances.mean(), arr1_pruned, arr2_pruned
+
+def extract_angle_arrays(data):
+    angle_corrs = {}
+
+    for stim in data.stimuli:
+        angle_corrs[stim] = {}
+
+        for s in data.data[data.data['Stimulus']==stim]['subjectID'].unique():
+            sub_df = data.data[(data.data['Stimulus']==stim) & (data.data['subjectID']==s)]
+            prev_x = sub_df['mean_x'].iloc[0]
+            prev_y = sub_df['mean_y'].iloc[0]
+
+            angles = []
+            for _, row in sub_df.iterrows():
+                angle = angle_from_horizontal(row, prev_x, prev_y)
+                angles.append(angle)
+                prev_x, prev_y = row['mean_x'], row['mean_y']
+
+            angle_corrs[stim][s] = angles
+    return angle_corrs
+
+def RSA_from_angles(angles_per_img):
+    rdm_angles_per_img = {}
+    for stim in angles_per_img:
+        RDMs = np.zeros((len(angles_per_img[stim]), len(angles_per_img[stim])))
+        for idx1, subj1 in enumerate(angles_per_img[stim]):
+            for idx2, subj2 in enumerate(angles_per_img[stim]):
+                #print(angle_corrs[stim][subj1])
+                corrs, _, _ =  prune_sort_corr_angles(np.abs(angles_per_img[stim][subj1]), np.abs(angles_per_img[stim][subj2]))
+                RDMs[idx1, idx2] = corrs
+                #print(np.abs(angle_corrs[stim][subj1]))
+                #print(np.abs(angle_corrs[stim][subj2]))
+
+    #print(RDMs)
+        rdm_angles_per_img[stim] = RDMs
+    for key, matrix in rdm_angles_per_img.items():
+        rdm_angles_per_img[key] = np.tril(matrix, -1)
+    return rdm_angles_per_img
