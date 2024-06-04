2960e6a0bb225805d267041c295bc400bbbd379e
Jozsef Arato <53469079+jozsarato@users.noreply.github.com>
2024-06-04 13:17:52 +0200

    Update _comparegroups.py
    infer size update

--- PyEyeSim/_comparegroups.py
+++ PyEyeSim/_comparegroups.py
@@ -529,7 +529,7 @@ def CompareGroupsMat(self,group,indsimmat):
         
 
 
-def CompareGroupsGridFix(self,Stim,betwcond,Conds=0,nhor=5,nver=5,cmap_ind='plasma',cmap_diff='RdYlBu',alpha=.5,t_abs=False,timemin=0, timemax=np.inf, timecol=0,useT=True,cutoff=-1): 
+def CompareGroupsGridFix(self,Stim,betwcond,Conds=0,nhor=5,nver=5,cmap_ind='plasma',cmap_diff='RdYlBu',alpha=.5,t_abs=False,timemin=0, timemax=np.inf, timecol=0,useT=True,cutoff=-1,InferS=2): 
     ''' 
 
     Stim: stimulus name
@@ -558,7 +558,7 @@ def CompareGroupsGridFix(self,Stim,betwcond,Conds=0,nhor=5,nver=5,cmap_ind='plas
         Conditions=np.copy(Conds)
       
    
-    statPMat,statEntropyMat=self.CalcStatPs(nhor,nver,MinFix=5,InferS=2,timemin=timemin, timemax=timemax, timecol=timecol)
+    statPMat,statEntropyMat=self.CalcStatPs(nhor,nver,MinFix=5,InferS=InferS,timemin=timemin, timemax=timemax, timecol=timecol)
 
    # if substring and len(stimn)==2:
 
